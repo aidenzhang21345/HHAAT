342d24b7937cb0c28d97af628020e283d978499e
tonylucy123 <tl123.kid@gmail.com>
2025-12-12 17:00:45 -0500

    read the notes comments in path1auton
    Co-authored-by: aidenzhang21345 <aidenzhang21345@users.noreply.github.com>
    Co-authored-by: melnslce <melnslce@users.noreply.github.com>
    Co-authored-by: HZ-kabo <HZ-kabo@users.noreply.github.com>
    Co-authored-by: stuy <lerbron67@users.noreply.github.com>

--- src/main/java/frc/robot/autons/Path1Auton.java
+++ src/main/java/frc/robot/autons/Path1Auton.java
@@ -8,26 +8,27 @@ import frc.robot.subsystems.Drivetrain;
 import frc.robot.commands.TurnDegrees;
 //import frc.robot.commands.TurnTime;
 import frc.robot.commands.DriveDistance;
-
-
+import frc.robot.commands.DriveTurn;
+//note the speed variable is supposed to be negative when reversing 
+//note 90 turned about 120 instead of 90; a speed of 1 is insanely fast and too unrealistic to control
 //bottom left or top right tony and henry
 //turndegrees: positive number is counterclockwise/to the left; negative number is clockwise/to the right
 public class Path1Auton extends SequentialCommandGroup {
     public Path1Auton(Drivetrain drivetrain) {
         addCommands( // assume the robot is facing out the exit of the base
-            new DriveDistance(1.0, 18.0, drivetrain), // double speed, double inches, Drivetrain drive
-            new TurnDegrees(1.0, -90.0, drivetrain), // 1 double speed, double deg, double inches, Drivetrain drivetrain
+            ///new DriveDistance(0.4, 18.0, drivetrain), // double speed, double inches, Drivetrain drive
+            new TurnDegrees(-0.4, 75.0, drivetrain) // 1 double speed, double deg, double inches, Drivetrain drivetrain
 
-            new DriveDistance(1.0, 11.0, drivetrain),
-            new TurnDegrees(1.0, 90.0, drivetrain), // 2
+           /*  new DriveDistance(0.4, 11.0, drivetrain),
+            new TurnDegrees(0.4, 90.0, drivetrain), // 2
 
-            new DriveDistance(1.0, 16.0, drivetrain),
-            new TurnDegrees(1.0, 90.0, drivetrain), // 3
+            new DriveDistance(0.4, 16.0, drivetrain),
+            new TurnDegrees(0.4, 90.0, drivetrain), // 3
 
-            new DriveDistance(1.0, 12.0, drivetrain),
-            new TurnDegrees(1.0, 70.0, drivetrain), // 4
+            new DriveDistance(0.4, 12.0, drivetrain),
+            new TurnDegrees(0.4, -70, drivetrain), // 4
 
-            new DriveDistance(1.0, 9.0, drivetrain) //5
+            new DriveDistance(0.4, 9.0, drivetrain) //5
             //add a comma if more commands
             //new -- continue function here if needed
             /* ROOKIES, INSERT DRIVE COMMANDS HERE TO CREATE YOUR AUTON! 
